0e939c346098e234970fe06f5827abafe12fd309
dhruvwarriorr <dhruvkumar31712@gmail.com>
2026-06-13 23:56:29 +0530

    cp

--- 8_misc/1_boilerplate.cpp
+++ 8_misc/1_boilerplate.cpp
@@ -1,8 +1,31 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 using ll = long long;
 const int MOD = 1e9 + 7;
+const int N = 3e5 + 5;
+const int LOG = 20;
+const ll INF = 4e18;
+
+void solve() {}
 
 int main() {
-    }
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  int t = 1;
+  // cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
